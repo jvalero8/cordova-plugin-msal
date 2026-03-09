f99275054aae6ac43594b7319b6ea0fbf33bc2d5
jvalero8 <jvalero.dev.app@gmail.com>
2026-03-09 21:02:16 +0100

    Update MsalPlugin.java

--- src/android/MsalPlugin.java
+++ src/android/MsalPlugin.java
@@ -259,7 +259,7 @@ public class MsalPlugin extends CordovaPlugin {
                         MsalPlugin.this.scopes = scopes.toArray(new String[0]);
                         MsalPlugin.this.isInit = true;
                         JSONObject dbgData = new JSONObject();
-                        dbgData.put("redirectURI", MsalPlugin.this.appSingleClient.getConfiguration().getRedirctUri());
+                        dbgData.put("redirectURI", MsalPlugin.this.appSingleClient.getConfiguration().getRedirectUri());
                         String serializedDbgData = dbgData.toString();
                         MsalPlugin.this.callbackContext.success(serializedDbgData);
                     } catch (JSONException ignored) {}
